722dba44bd48dbb5f316e59b01d5f5a97a51b875
dincho <dincho@users.noreply.github.com>
2014-02-28 23:10:20 +0200

    Initial Commit from XCode

---
+++ statix/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  statix
+//
+//  Created by Dincho Todorov on 2/28/14.
+//  Copyright (c) 2014 Dincho Todorov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
