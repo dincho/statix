669f71e21d48e8bff662f81cc68de353f7b5498c
dincho <dincho@users.noreply.github.com>
2014-08-28 17:02:32 +0300

    accept multiple connections at once

--- statix/stx_accept.c
+++ statix/stx_accept.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <unistd.h> //getpid, close
+#include <errno.h>
 
 #include <netinet/in.h> /* For sockaddr_in */
 #include <sys/socket.h> /* For socket functions */
@@ -31,37 +32,40 @@ void stx_accept(int queue, stx_server_t *server)
     char port[NI_MAXSERV];
     stx_request_t *request;
     
-    conn = accept(server->sock, (struct sockaddr*)&ss, &slen);
-    if (conn < 0) {
-        perror("accept");
+    for (;;) {
+        conn = accept(server->sock, NULL, 0);
+        if (conn < 0) {
+            if (errno != EAGAIN) {
+                perror("accept");
+            }
+            
+            break;
+        }
+        
+        if (-1 == fcntl(conn, F_SETFL, fcntl(conn, F_GETFL, 0) | O_NONBLOCK)) {
+            perror("fcntl");
+            close(conn);
+            
+            break;
+        }
 
-        return;
+        rc = getnameinfo((struct sockaddr *)&ss, slen,
+                             host, sizeof(host), port, sizeof(port),
+                             NI_NUMERICHOST | NI_NUMERICSERV);
+        if (rc == 0) {
+            stx_log(server->logger, STX_LOG_INFO,
+                    "Accepted connection from %s:%s", host, port);
+        }
+        
+        request = stx_init_request(server, conn);
+        if (NULL == request) {
+            stx_log(server->logger, STX_LOG_ERR,
+                    "Error while initializing request");
+            close(conn);
+            
+            break;
+        }
+        
+        stx_event(queue, conn, STX_EV_READ, request);
     }
-    
-    if (-1 == fcntl(conn, F_SETFL, fcntl(conn, F_GETFL, 0) | O_NONBLOCK)) {
-        perror("fcntl");
-        close(conn);
-
-        return;
-    }
-    
-    //getaddrinfo ?
-    rc = getnameinfo((struct sockaddr *)&ss, slen,
-                         host, sizeof(host), port, sizeof(port),
-                         NI_NUMERICHOST | NI_NUMERICSERV);
-    if (rc == 0) {
-        stx_log(server->logger, STX_LOG_INFO,
-                "Accepted connection from %s:%s", host, port);
-    }
-    
-    request = stx_init_request(server, conn);
-    if (NULL == request) {
-        stx_log(server->logger, STX_LOG_ERR,
-                "Error while initializing request");
-        close(conn);
-
-        return;
-    }
-
-    stx_event(queue, conn, STX_EV_READ, request);
 }
