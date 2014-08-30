d1a48a22b3a9a44f6ac0fc1420f3b506f6a14d45
dincho <dincho@users.noreply.github.com>
2014-08-30 12:48:19 +0300

    cleanup file processing function

--- statix/stx_request.c
+++ statix/stx_request.c
@@ -12,6 +12,7 @@
 #include <fcntl.h> //O_RDONLY
 #include <sys/stat.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "stx_request.h"
 #include "stx_log.h"
@@ -215,30 +216,19 @@ int stx_request_parse_line(stx_request_t *r)
 
 void stx_request_process_file(stx_request_t *r)
 {
-    size_t namelen;
-    char *filename;
     int fd;
+    char filepath[PATH_MAX];
     struct stat sb;
     
-    if (r->method != STX_METHOD_GET) {
-        r->status = STX_STATUS_NOT_IMPL;
-        return;
-    }
+    strcpy(filepath, r->server->webroot);
+    strncat(filepath, r->uri_start, r->uri_len);
     
-    namelen = strlen(r->server->webroot) + r->uri_len;
+    //append default index file when / is requested
     if (*(r->uri_start + r->uri_len - 1) == '/') {
-        namelen += strlen(r->server->index);
+        strncat(filepath, r->server->index, strlen(r->server->index));
     }
     
-    filename = malloc(namelen * sizeof(char));
-    strcpy(filename, r->server->webroot);
-    strncat(filename, r->uri_start, r->uri_len);
-    
-    if (*(r->uri_start + r->uri_len - 1) == '/') {
-        strncat(filename, r->server->index, strlen(r->server->index));
-    }
-    
-    if ((fd = open(filename, O_RDONLY)) == -1) {
+    if ((fd = open(filepath, O_RDONLY)) == -1) {
         if (ENOENT == errno) {
             r->status = STX_STATUS_NOT_FOUND;
         } else if (EACCES == errno) {
@@ -248,31 +238,19 @@ void stx_request_process_file(stx_request_t *r)
             perror("open");
         }
         
-        free(filename);
-        
         return;
     }
     
     if (fstat(fd, &sb) == -1) {
         r->status = STX_STATUS_ERROR;
         perror("fstat");
-        free(filename);
         
         return;
     }
     
-    
-    free(filename);
-    
-    //sb.mtime
-    //content type
-    //charset ?
-    //date
     r->status = STX_STATUS_OK;
     r->content_length = sb.st_size;
     r->fd = fd;
-    
-    return;
 }
 
 void stx_request_set_content_type(stx_request_t *r)
